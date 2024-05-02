0eb8e2353cc73ec4d307b3a9de9be1e663ba2ab3
kimaita <johnnieqaym@gmail.com>
2024-05-02 03:05:43 +0300

    added stat endpoint for total object counts

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
 """defines routes for the API"""
 from api.v1.views import app_views
+from api.v1.app import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
 def get_status():
     """Returns a status OK message"""
     return {'status': 'OK'}
+
+
+@app_views.route('/stats')
+def get_object_stats():
+    """Retrieves object counts by type"""
+    classes = {'amenities': Amenity, 'cities': City, 'places': Place,
+               'reviews': Review, 'states': State, 'users': User}
+
+    objs = {k: storage.count(v) for k, v in classes.items()}
+    return objs
