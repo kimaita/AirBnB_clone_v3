38d23154a520a488dfe028e099188eeed0e91fa9
kimaita <johnnieqaym@gmail.com>
2024-05-02 03:30:29 +0300

    404 error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,13 +12,18 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_storage(exception):
-    """Closes connection to the storage engine on request response.
-    """
+    """Closes connection to the storage engine on request response."""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Returns a json response for 404 errors"""
+    return {'error': 'Not found'}, 404
 
 
 if __name__ == "__main__":
 
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', '5000')
-    app.run(host, port, threaded=True)
+    app.run(host, port, threaded=True, debug=True)
