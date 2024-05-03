37cab9f846431fd07607f096551a4d59c95407cb
kimaita <johnnieqaym@gmail.com>
2024-05-03 04:13:29 +0300

    added amenities andpoint

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,10 +6,13 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 states = Blueprint('states', __name__, url_prefix='/states')
 cities = Blueprint('cities', __name__, url_prefix='/cities')
+amenities = Blueprint('amenities', __name__, url_prefix='/amenities')
 
 app_views.register_blueprint(states)
 app_views.register_blueprint(cities)
+app_views.register_blueprint(amenities)
 
+from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.index import *
 from api.v1.views.states import *
---
+++ api/v1/views/amenities.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""Handles REST requests for City objects"""
+
+from api.v1.app import storage
+from api.v1.views import amenities
+from models.amenity import Amenity
+from flask import jsonify, request, abort
+
+
+@amenities.route('/', methods=['GET'], strict_slashes=False)
+def get_all_amenities():
+    """Returns a list of all amenities"""
+    amenities = storage.all(Amenity)
+
+    return jsonify([obj.to_dict() for obj in amenities.values()])
+
+
+@amenities.route('/<amenity_id>', methods=['GET'], strict_slashes=False)
+def get_amenity(amenity_id):
+    """Returns a single Amenity by id"""
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+
+    return amenity.to_dict()
+
+
+@amenities.route('/', methods=['POST'], strict_slashes=False)
+def create_amenity():
+    """Creates a new Amenity from POST data and persists it in storage
+    """
+    amenity_dict = request.get_json()
+    if not amenity_dict:
+        abort(400, 'Not a JSON')
+    if 'name' not in amenity_dict:
+        abort(400, 'Missing name')
+
+    amenity = Amenity(**amenity_dict)
+    amenity.save()
+    return amenity.to_dict(), 201
+
+
+@amenities.route('/<amenity_id>', methods=['PUT'], strict_slashes=False)
+def update_amenity(amenity_id):
+    """Updates Amenity properties"""
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+
+    amenity_dict = request.get_json()
+    if not amenity_dict:
+        abort(400, 'Not a JSON')
+
+    ignore = ['id', 'created_at', 'updated_at']
+
+    for attr, val in amenity_dict.items():
+        if attr not in ignore:
+            setattr(amenity, attr, val)
+    amenity.save()
+
+    return amenity.to_dict(), 200
+
+
+@amenities.route('/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+def delete_amenity(amenity_id):
+    """Deletes an amenity, returning an empty dict"""
+
+    obj = storage.get(Amenity, amenity_id)
+    if obj is None:
+        abort(404)
+
+    storage.delete(obj)
+    storage.save()
+    return {}, 200
