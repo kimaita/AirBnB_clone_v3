1ff5370dc1b95a3d22426c57e2d64109b71a7f3c
kimaita <johnnieqaym@gmail.com>
2024-05-03 03:49:45 +0300

    added cities endpoint

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Instantiates the app_views Blueprint"""
+"""Instantiates flask Blueprints"""
+
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 states = Blueprint('states', __name__, url_prefix='/states')
-app_views.register_blueprint(states)
+cities = Blueprint('cities', __name__, url_prefix='/cities')
 
+app_views.register_blueprint(states)
+app_views.register_blueprint(cities)
+
+from api.v1.views.cities import *
 from api.v1.views.index import *
 from api.v1.views.states import *
---
+++ api/v1/views/cities.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""Handles REST requests for City objects"""
+
+from api.v1.app import storage
+from api.v1.views import states, cities
+from models.state import State
+from models.city import City
+from flask import jsonify, request, abort
+
+
+@cities.route('/<city_id>', methods=['GET'], strict_slashes=False)
+def get_city(city_id):
+    """Returns a single City by id"""
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+
+    return city.to_dict()
+
+
+@states.route('/<state_id>/cities', methods=['GET'], strict_slashes=False)
+def get_all_state_cities(state_id):
+    """Returns a list of all cities for the State"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+
+    objs = [obj.to_dict() for obj in state.cities]
+    return jsonify(objs)
+
+
+@states.route('/<state_id>/cities', methods=['POST'], strict_slashes=False)
+def create_city(state_id):
+    """Creates a new City from POST data and persists it in storage
+    """
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+
+    city_dict = request.get_json()
+    if not city_dict:
+        abort(400, 'Not a JSON')
+    if 'name' not in city_dict:
+        abort(400, 'Missing name')
+
+    city = City(**city_dict)
+    city.state_id = state.id
+    city.save()
+    return city.to_dict(), 201
+
+
+@cities.route('/<city_id>', methods=['PUT'], strict_slashes=False)
+def update_city(city_id):
+    """Updates City object properties"""
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+
+    city_dict = request.get_json()
+    if not city_dict:
+        abort(400, 'Not a JSON')
+
+    ignore = ['id', 'state_id', 'created_at', 'updated_at']
+
+    for attr, val in city_dict.items():
+        if attr not in ignore:
+            setattr(city, attr, val)
+    city.save()
+
+    return city.to_dict(), 200
+
+
+@cities.route('/<city_id>', methods=['DELETE'], strict_slashes=False)
+def delete_city(city_id):
+    """Deletes a City object, returning an empty dict"""
+
+    obj = storage.get(City, city_id)
+    if obj is None:
+        abort(404)
+
+    storage.delete(obj)
+    storage.save()
+    return {}, 200
